2bf4ec03aff04211da89b2b9b752f482fda1d03e
Luan@YokKok <97419663+LuanPhan2911@users.noreply.github.com>
2025-06-09 21:50:05 +0700

    hot fix: xoa approval true

--- backend/src/services/exam.service.js
+++ backend/src/services/exam.service.js
@@ -51,13 +51,13 @@ const examService = {
   getExamsByUserId: async (userId) => {
     return await prisma.exam.findMany({
       where: { createdById: userId },
-      include: { createdBy: true, approval: true },
+      include: { createdBy: true },
     });
   },
 
   getAllExams: async () => {
     return await prisma.exam.findMany({
-      include: { createdBy: true, approval: true },
+      include: { createdBy: true },
     });
   },
 
